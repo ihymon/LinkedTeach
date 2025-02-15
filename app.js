4ee7ee19957f22c9b70a19cfece7983f59b31772
MuhaAndriy <dipount@gmail.com>
2025-02-15 10:01:33 +0200

    creat get/metod

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+
