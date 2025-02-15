80ee1683491ac27716235b35ec8155a106fb289a
MuhaAndriy <dipount@gmail.com>
2025-02-15 09:58:34 +0200

    creat server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
